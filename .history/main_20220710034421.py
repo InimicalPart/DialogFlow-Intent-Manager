2118a07106c5b39d0ab68371cfb33ebe09b55caa
InimicalPart <inimicalpart@gmail.com>
2022-07-10 18:04:43 +0200

    fixing oopsie

--- .history/main_20220710034421.py
+++
@@ -1,2 +0,0 @@
-from google.api_core.exceptions import InvalidArgument
-import google.cloud.dialogflow_v2 as dialogflow
--- .history/main_20220710034550.py
+++
@@ -1,5 +0,0 @@
-from google.api_core.exceptions import InvalidArgument
-import google.cloud.dialogflow_v2 as dialogflow
-import os
-import json
-config = json.load(open('config.json'))
